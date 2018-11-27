8b98eccaf055c20fbd5a51b83458d3ed93985d25
Calamarikitty23 <skyekr@byui.edu>
2018-11-26 17:38:34 -0700

    website

---
+++ assignments/weathersite/scripts/towndata.js
@@ -0,0 +1,47 @@
+    var article = document.querySelector('article');
+    var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+    var idaho= request.response;
+    show(idaho);
+    
+}
+
+  function show(jsonObj) {
+    var data = jsonObj['towns'];
+      
+  for (var i = 0; i < data.length; i++){
+     if ( i === 0) {continue;}
+     if ( i === 2) {continue;}
+     if ( i === 3) {continue;}
+     if ( i === 6) {continue;}
+
+    var myArticle = document.createElement('div');
+    var myH2 = document.createElement('h3');
+    var myPara1 = document.createElement('p');
+    var myPara2 = document.createElement('p');
+    var myPara3 = document.createElement('p');
+    var myPara4 = document.createElement('p');
+    var myImg = document.createElement('img');
+
+    myH2.textContent = data[i].name;
+    myPara1.textContent = data[i].motto;
+    myPara2.textContent = 'Year Founded:' + data[i].yearFounded;
+    myPara3.textContent = 'Population:'+ data[i].currentPopulation;
+    myPara4.textContent = 'Average Rain Fall:' + data[i].averageRainfall;
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myPara4);
+    myArticle.appendChild(myImg);
+    if (i === 1) myImg.src="images/fishhaven.jpg";
+    if (i === 4) myImg.src="images/downtownpreston.jpg";
+    if (i === 5) myImg.src="images/geyser.jpg";
+    article.appendChild(myArticle);
+  }
+}
---
+++ assignments/weathersite/scripts/weather.js
@@ -0,0 +1,40 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a996fa0bda7d363240d97a43b64c13cc&units=imperial');
+request.send();
+request.onload = function() {
+var preston = JSON.parse(request.responseText);
+console.log(preston);
+
+document.getElementById('current').innerHTML=preston.weather[0].description;
+document.getElementById('temp').innerHTML=preston.main.temp;
+document.getElementById("temp").innerHTML = Math.round(preston.main.temp) + "" + "°F";
+document.getElementById('humid').innerHTML=preston.main.humidity + "" + "%";
+document.getElementById('speedy').innerHTML=preston.wind.speed+ "" + "mph";
+document.getElementById('windy').innerHTML=preston.wind.deg;
+
+var iconcode=preston.weather[0].icon;
+var lightbulb="//openweathermap.org/img/w/" + iconcode + '.png';
+document.getElementById('lightbulb').src=lightbulb;
+
+}
+
+var requester = new XMLHttpRequest();
+requester.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=a996fa0bda7d363240d97a43b64c13cc&units=imperial');
+requester.send();
+requester.onload = function() {
+var preston = JSON.parse(requester.responseText);
+console.log(preston);
+
+document.getElementById('monday').innerHTML=preston.list[1].main.temp;
+document.getElementById("monday").innerHTML = Math.round(preston.list[1].main.temp) + "" + "°F";
+document.getElementById('tuesday').innerHTML=preston.list[2].main.temp+ "" + "°F";
+document.getElementById("tuesday").innerHTML = Math.round(preston.list[2].main.temp) + "" + "°F";
+document.getElementById('wednesday').innerHTML=preston.list[3].main.temp+ "" + "°F";
+document.getElementById("wednesday").innerHTML = Math.round(preston.list[3].main.temp) + "" + "°F";
+document.getElementById('thursday').innerHTML=preston.list[4].main.temp+ "" + "°F";
+document.getElementById("thursday").innerHTML = Math.round(preston.list[4].main.temp) + "" + "°F";
+document.getElementById('friday').innerHTML=preston.list[5].main.temp+ "" + "°F";
+document.getElementById("friday").innerHTML = Math.round(preston.list[5].main.temp) + "" + "°F";
+
+
+}
