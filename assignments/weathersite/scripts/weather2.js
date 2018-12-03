020d27f03edd91e6f1d9fe08a05c15a6744efb1c
Calamarikitty23 <skyekr@byui.edu>
2018-12-03 09:50:47 -0700

    Weather 1

--- assignments/weathersite/scripts/weather2.js
+++ assignments/weathersite/scripts/weather2.js
@@ -2,17 +2,17 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=a996fa0bda7d363240d97a43b64c13cc&units=imperial');
 request.send();
 request.onload = function() {
-var preston = JSON.parse(request.responseText);
-console.log(preston);
+var sodasprings = JSON.parse(request.responseText);
+console.log(sodasprings);
 
-document.getElementById('current').innerHTML=preston.weather[0].description;
-document.getElementById('temp').innerHTML=preston.main.temp;
-document.getElementById("temp").innerHTML = Math.round(preston.main.temp) + "" + "°F";
-document.getElementById('humid').innerHTML=preston.main.humidity + "" + "%";
-document.getElementById('speedy').innerHTML=preston.wind.speed+ "" + "mph";
-document.getElementById('windy').innerHTML= Math.round(preston.wind.deg);
+document.getElementById('current').innerHTML=sodasprings.weather[0].description;
+document.getElementById('temp').innerHTML=sodasprings.main.temp;
+document.getElementById("temp").innerHTML = Math.round(sodasprings.main.temp) + "" + "°F";
+document.getElementById('humid').innerHTML=sodasprings.main.humidity + "" + "%";
+document.getElementById('speedy').innerHTML=sodasprings.wind.speed+ "" + "mph";
+document.getElementById('windy').innerHTML= Math.round(sodasprings.wind.deg);
 
-var iconcode=preston.weather[0].icon;
+var iconcode=sodasprings.weather[0].icon;
 var lightbulb="//openweathermap.org/img/w/" + iconcode + '.png';
 document.getElementById('lightbulb').src=lightbulb;
 
@@ -22,19 +22,19 @@ var requester = new XMLHttpRequest();
 requester.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=a996fa0bda7d363240d97a43b64c13cc&units=imperial');
 requester.send();
 requester.onload = function() {
-var preston = JSON.parse(requester.responseText);
-console.log(preston);
+var sodasprings = JSON.parse(requester.responseText);
+console.log(sodasprings);
 
-document.getElementById('monday').innerHTML=preston.list[1].main.temp;
-document.getElementById("monday").innerHTML = Math.round(preston.list[1].main.temp) + "" + "°F";
-document.getElementById('tuesday').innerHTML=preston.list[2].main.temp+ "" + "°F";
-document.getElementById("tuesday").innerHTML = Math.round(preston.list[2].main.temp) + "" + "°F";
-document.getElementById('wednesday').innerHTML=preston.list[3].main.temp+ "" + "°F";
-document.getElementById("wednesday").innerHTML = Math.round(preston.list[3].main.temp) + "" + "°F";
-document.getElementById('thursday').innerHTML=preston.list[4].main.temp+ "" + "°F";
-document.getElementById("thursday").innerHTML = Math.round(preston.list[4].main.temp) + "" + "°F";
-document.getElementById('friday').innerHTML=preston.list[5].main.temp+ "" + "°F";
-document.getElementById("friday").innerHTML = Math.round(preston.list[5].main.temp) + "" + "°F";
+document.getElementById('monday').innerHTML=sodasprings.list[1].main.temp;
+document.getElementById("monday").innerHTML = Math.round(sodasprings.list[1].main.temp) + "" + "°F";
+document.getElementById('tuesday').innerHTML=sodasprings.list[2].main.temp+ "" + "°F";
+document.getElementById("tuesday").innerHTML = Math.round(sodasprings.list[2].main.temp) + "" + "°F";
+document.getElementById('wednesday').innerHTML=sodasprings.list[3].main.temp+ "" + "°F";
+document.getElementById("wednesday").innerHTML = Math.round(sodasprings.list[3].main.temp) + "" + "°F";
+document.getElementById('thursday').innerHTML=sodasprings.list[4].main.temp+ "" + "°F";
+document.getElementById("thursday").innerHTML = Math.round(sodasprings.list[4].main.temp) + "" + "°F";
+document.getElementById('friday').innerHTML=sodasprings.list[5].main.temp+ "" + "°F";
+document.getElementById("friday").innerHTML = Math.round(sodasprings.list[5].main.temp) + "" + "°F";
 
 
 }
--- assignments/weathersite/scripts/weather3.js
+++ assignments/weathersite/scripts/weather3.js
@@ -2,17 +2,17 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=a996fa0bda7d363240d97a43b64c13cc&units=imperial');
 request.send();
 request.onload = function() {
-var preston = JSON.parse(request.responseText);
-console.log(preston);
+var fishhaven = JSON.parse(request.responseText);
+console.log(fishhaven);
 
-document.getElementById('current').innerHTML=preston.weather[0].description;
-document.getElementById('temp').innerHTML=preston.main.temp;
-document.getElementById("temp").innerHTML = Math.round(preston.main.temp) + "" + "°F";
-document.getElementById('humid').innerHTML=preston.main.humidity + "" + "%";
-document.getElementById('speedy').innerHTML=preston.wind.speed+ "" + "mph";
-document.getElementById('windy').innerHTML= Math.round(preston.wind.deg);
+document.getElementById('current').innerHTML=fishhaven.weather[0].description;
+document.getElementById('temp').innerHTML=fishhaven.main.temp;
+document.getElementById("temp").innerHTML = Math.round(fishhaven.main.temp) + "" + "°F";
+document.getElementById('humid').innerHTML=fishhaven.main.humidity + "" + "%";
+document.getElementById('speedy').innerHTML=fishhaven.wind.speed+ "" + "mph";
+document.getElementById('windy').innerHTML= Math.round(fishhaven.wind.deg);
 
-var iconcode=preston.weather[0].icon;
+var iconcode=fishhaven.weather[0].icon;
 var lightbulb="//openweathermap.org/img/w/" + iconcode + '.png';
 document.getElementById('lightbulb').src=lightbulb;
 
@@ -22,19 +22,19 @@ var requester = new XMLHttpRequest();
 requester.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5585010&appid=a996fa0bda7d363240d97a43b64c13cc&units=imperial');
 requester.send();
 requester.onload = function() {
-var preston = JSON.parse(requester.responseText);
-console.log(preston);
+var fishhaven = JSON.parse(requester.responseText);
+console.log(fishhaven);
 
-document.getElementById('monday').innerHTML=preston.list[1].main.temp;
-document.getElementById("monday").innerHTML = Math.round(preston.list[1].main.temp) + "" + "°F";
-document.getElementById('tuesday').innerHTML=preston.list[2].main.temp+ "" + "°F";
-document.getElementById("tuesday").innerHTML = Math.round(preston.list[2].main.temp) + "" + "°F";
-document.getElementById('wednesday').innerHTML=preston.list[3].main.temp+ "" + "°F";
-document.getElementById("wednesday").innerHTML = Math.round(preston.list[3].main.temp) + "" + "°F";
-document.getElementById('thursday').innerHTML=preston.list[4].main.temp+ "" + "°F";
-document.getElementById("thursday").innerHTML = Math.round(preston.list[4].main.temp) + "" + "°F";
-document.getElementById('friday').innerHTML=preston.list[5].main.temp+ "" + "°F";
-document.getElementById("friday").innerHTML = Math.round(preston.list[5].main.temp) + "" + "°F";
+document.getElementById('monday').innerHTML=fishhaven.list[1].main.temp;
+document.getElementById("monday").innerHTML = Math.round(fishhaven.list[1].main.temp) + "" + "°F";
+document.getElementById('tuesday').innerHTML=fishhaven.list[2].main.temp+ "" + "°F";
+document.getElementById("tuesday").innerHTML = Math.round(fishhaven.list[2].main.temp) + "" + "°F";
+document.getElementById('wednesday').innerHTML=fishhaven.list[3].main.temp+ "" + "°F";
+document.getElementById("wednesday").innerHTML = Math.round(fishhaven.list[3].main.temp) + "" + "°F";
+document.getElementById('thursday').innerHTML=fishhaven.list[4].main.temp+ "" + "°F";
+document.getElementById("thursday").innerHTML = Math.round(fishhaven.list[4].main.temp) + "" + "°F";
+document.getElementById('friday').innerHTML=fishhaven.list[5].main.temp+ "" + "°F";
+document.getElementById("friday").innerHTML = Math.round(fishhaven.list[5].main.temp) + "" + "°F";
 
 
 }
