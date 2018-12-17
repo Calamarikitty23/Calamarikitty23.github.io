2e6f1eda205e0d0ff330f189b5661724bdf524e0
Calamarikitty23 <skyekr@byui.edu>
2018-12-17 16:15:32 -0700

    Temple 9

--- templeinn/scripts/data.js
+++ templeinn/scripts/data.js
@@ -1,5 +1,5 @@
     var article = document.querySelector('article');
-    var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+    var requestURL = 'https://www.lds.org/temples/list?lang=eng/data.json';
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
@@ -11,7 +11,7 @@
 }
 
   function show(jsonObj) {
-    var data = jsonObj['towns'];
+    var data = jsonObj['temples'];
       
   for (var i = 0; i < data.length; i++) {
      if ( i === 0) {continue;}
@@ -25,19 +25,34 @@
     var myPara2 = document.createElement('p');
     var myPara3 = document.createElement('p');
     var myPara4 = document.createElement('p');
+    var myPara5 = document.createElement('p');
+    var myPara6 = document.createElement('p');
+    var myPara7 = document.createElement('p');
+    var myPara8 = document.createElement('p');
+    var myPara9 = document.createElement('p');
     var myImg = document.createElement('img');
 
     myH2.textContent = data[i].name;
-    myPara1.textContent = data[i].motto;
-    myPara2.textContent = 'Year Founded:' + data[i].yearFounded;
-    myPara3.textContent = 'Population:'+ data[i].currentPopulation;
-    myPara4.textContent = 'Average Rain Fall:' + data[i].averageRainfall;
+    myPara1.textContent = 'Address:' + data[i].address;
+    myPara2.textContent = 'Telephone:' + data[i].telephoe;
+    myPara3.textContent = 'Email:'+ data[i].email;
+    myPara4.textContent = 'Services:' + data[i].services;
+    myPara5.textContent = 'History:' + data[i].history;
+    myPara6.textContent = 'Ordinance Schedule:'+ data[i].ordinanceSchedule;
+    myPara7.textContent = 'Session Schedule:'+ data[i].sessionSchedule;
+    myPara8.textContent = 'Temple Closure Schedule:'+ data[i].templeclosureSchedule;
+    myPara9.textContent = 'Current Conditions Weather:'+ data[i].currentConditions;
 
     myArticle.appendChild(myH2);
     myArticle.appendChild(myPara1);
     myArticle.appendChild(myPara2);
     myArticle.appendChild(myPara3);
     myArticle.appendChild(myPara4);
+    myArticle.appendChild(myPara5);
+    myArticle.appendChild(myPara6);
+    myArticle.appendChild(myPara7);
+    myArticle.appendChild(myPara8);
+    myArticle.appendChild(myPara9);
     myArticle.appendChild(myImg);
     if (i === 1) myImg.src="images/";
     if (i === 4) myImg.src="images/";
@@ -45,3 +60,6 @@
     article.appendChild(myArticle);
   }
 }
+/*<!--The temple page describes the details for at least four temples providing information about the address,
+     telephone, email, services, history, ordinance schedule, session schedule, temple closure schedule, 
+     and current conditions weather information obtained from a third party API.-->*/
