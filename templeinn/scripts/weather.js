7f035a7e5e8c661910f4945e0ebdb4ad3da1629d
Calamarikitty23 <skyekr@byui.edu>
2018-12-18 18:04:33 -0700

    Temple 12

--- templeinn/scripts/weather.js
+++ templeinn/scripts/weather.js
@@ -20,19 +20,3 @@ var temper = preston.main.temp;
 var blizzard= 35.74 + 0.6215 * temper - 35.75 * Math.pow(wind,0.16) + 0.4275 * temper * Math.pow(wind,0.16);
 document.getElementById("windy").innerHTML= Math.round(blizzard);
 }
-
-var requester = new XMLHttpRequest();
-requester.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=a996fa0bda7d363240d97a43b64c13cc&units=imperial');
-requester.send();
-requester.onload = function() {
-var preston = JSON.parse(requester.responseText);
-console.log(preston);
-
-document.getElementById("monday").innerHTML = Math.round(preston.list[6].main.temp) + "" + "°F";
-document.getElementById("tuesday").innerHTML = Math.round(preston.list[14].main.temp) + "" + "°F";
-document.getElementById("wednesday").innerHTML = Math.round(preston.list[22].main.temp) + "" + "°F";
-document.getElementById("thursday").innerHTML = Math.round(preston.list[30].main.temp) + "" + "°F";
-document.getElementById("friday").innerHTML = Math.round(preston.list[38].main.temp) + "" + "°F";
-
-
-}
